e788e822d1aad150596c3639bef16dcfdb44e823
daviponteslopes12-web <daviponteslopes12@gmail.com>
2026-07-23 16:57:57 -0300

    feat: conexão com o banco e configurando servidor backend

---
+++ ProjetoHamburgueria/backend/src/config/db.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv/config';
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+export default pool;
