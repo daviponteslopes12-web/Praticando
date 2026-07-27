18422308f95bca596e7813d3deab173cde7b63f7
daviponteslopes12-web <daviponteslopes12@gmail.com>
2026-07-27 19:33:36 -0300

    feat: repository e service de combo

---
+++ ProjetoHamburgueria/backend/src/repositories/comboRepository.js
@@ -0,0 +1,158 @@
+import db from "../config/database.js";
+
+const comboRepository = {
+    async cadastrarCombo(nome, preco, produtos) {
+        const [resultado] = await db.query(
+            `
+            INSERT 
+            INTO combos 
+            (nome, preco) 
+            VALUES (?, ?)
+            `,
+            [nome, preco],
+        );
+
+        const comboId = resultado.insertId;
+
+        for (const produtoId of produtos) {
+            await db.query(
+                `
+                    INSERT INTO combo_itens (combo_id, produto_id)
+                    VALUES (?, ?)
+                    `,
+                [comboId, produtoId],
+            );
+        }
+
+        return comboId;
+    },
+
+    async listarCombo() {
+        const [combos] = await db.query(`
+            SELECT * FROM combos`);
+
+        for (const combo of combos) {
+            combo.produtos = await this.buscarProdutosDoCombo(combo.id);
+        }
+
+        return combos;
+    },
+
+    async listarComboAtivo() {
+        const [combos] = await db.query(`
+            SELECT *
+            FROM combos 
+            WHERE ativo = true`);
+
+        for (const combo of combos) {
+            combo.produtos = await this.buscarProdutosDoCombo(combo.id);
+        }
+
+        return combos;
+    },
+
+    async buscarPorId(id) {
+        const [resultado] = await db.query(
+            `
+            SELECT *
+            FROM combos
+            WHERE id = ?
+            `,
+            [id],
+        );
+
+        return resultado[0] || null;
+    },
+
+    async buscarProdutosDoCombo(comboId) {
+        const [resultado] = await db.query(
+            `
+            SELECT p.id, p.nome, p.preco, p.categoria, p.imagem
+            FROM combo_itens ci
+            JOIN produtos p ON p.id = ci.produto_id
+            WHERE ci.combo_id = ?
+            `,
+            [comboId],
+        );
+
+        return resultado;
+    },
+
+    async deletarCombo(id) {
+        await db.query(
+            `
+            DELETE FROM combo_itens
+            WHERE combo_id = ?
+            `,
+            [id],
+        );
+
+        const [resultado] = await db.query(
+            `
+            DELETE FROM combos
+            WHERE id = ?
+            `,
+            [id],
+        );
+
+        return resultado.affectedRows;
+    },
+
+    async editarCombo(id, nome, preco, produtos) {
+        await db.query(
+            `
+            UPDATE combos
+            SET nome = ?, preco = ?
+            WHERE id = ?
+            `,
+            [nome, preco, id],
+        );
+
+        await db.query(
+            `
+            DELETE FROM combo_itens 
+            WHERE combo_id = ?
+            `,
+            [id],
+        );
+
+        for (const produtoId of produtos) {
+            await db.query(
+                `
+                INSERT INTO combo_itens
+                (combo_id, produto_id)
+                VALUES (?, ?)
+                `,
+                [id, produtoId],
+            );
+        }
+
+        return 1;
+    },
+
+    async alternarAtivo(id, ativo) {
+        const [resultado] = await db.query(
+            `
+            UPDATE combos
+            SET ativo = ?
+            WHERE id = ?
+            `,
+            [ativo, id],
+        );
+
+        return resultado.affectedRows;
+    },
+
+    async buscarPorNome(nome) {
+        const [resultado] = await db.query(
+            `
+        SELECT * FROM combos WHERE nome = ?
+    `,
+            [nome],
+        );
+
+        return resultado[0] || null;
+    },
+};
+
+export default comboRepository;
---
+++ ProjetoHamburgueria/backend/src/services/comboService.js
@@ -0,0 +1,93 @@
+import comboRepository from "../repositories/comboRepository.js";
+import produtoRepository from '../repositories/produtoRepository.js';
+import criarErro from '../utils/criarErro.js';
+
+const comboService = {
+    async cadastrarCombo(nome, preco, produtos) {
+        const comboExistente = await comboRepository.buscarPorNome(nome);
+
+        if (comboExistente) {
+            throw criarErro("Combo já cadastrado", 400);
+        }
+
+        for (const produtoId of produtos) {
+            const produto = await produtoRepository.buscarPorId(produtoId)
+        }
+
+        if (!produto) {
+            throw criarErro(`Produto com id ${produtoId} não encontrado`, 404);
+        }
+
+        const id = await comboRepository.cadastrarCombo(nome, preco, produtos);
+
+        return { id, nome, preco, produtos };
+    },
+
+    async listarCombos() {
+        const combos = await comboRepository.listarComboAtivo();
+        return combos;
+    },
+
+    async listarComboAtivo() {
+        const combos = await comboRepository.listarComboAtivo();
+        return combos;
+    },
+
+    async editarCombo(id, nome, preco, produtos) {
+        const combo = await comboRepository.buscarPorId(id);
+
+        if (!combo) {
+            throw criarErro("Combo não encontrado", 404);
+        }
+
+        if (nome) {
+            const nomeExistente = await comboRepository.buscarPorNome(nome);
+
+            if (nomeExistente && nomeExistente.id != id) {
+                throw criarErro("Já existe um combo com esse nome", 400);
+            }
+        }
+
+        for (const produtoId of produtos) {
+            const produto = await produtoRepository.buscarPorId(produtoId);
+
+            if (!produto) {
+                throw criarErro(`Produto com id ${produtoId} não encontrado`, 404);
+            }
+        }
+
+        await comboRepository.editarCombo(id, nome, preco, produtos);
+
+        const comboAtualizado = await comboRepository.buscarPorId(id);
+        comboAtualizado.produtos = await comboRepository.buscarProdutosDoCombo(id);
+
+        return comboAtualizado;
+    },
+
+    async deletarCombo(id) {
+        const combo = await comboRepository.buscarPorId(id);
+
+        if (!combo) {
+            throw criarErro("Combo não encontrado", 404);
+        }
+
+        await comboRepository.deletarCombo(id);
+    },
+
+    async alternarAtivo(id, ativo) {
+        const combo = await comboRepository.buscarPorId(id);
+
+        if (!combo) {
+            throw criarErro("Combo não encontrado", 404);
+        }
+
+        await comboRepository.alternarAtivo(id, ativo);
+
+        const comboAtualizado = await comboRepository.buscarPorId(id);
+        comboAtualizado.produtos = await comboRepository.buscarProdutosDoCombo(id);
+
+        return comboAtualizado;
+    },
+}
+
+export default comboService;
