be5b1f922319f43595d979dca82dd6d2fb2a7002
daviponteslopes12-web <daviponteslopes12@gmail.com>
2026-07-30 16:00:45 -0300

    feat: integrando QueryClient e react-router no componente main

--- ProjetoHamburgueria/frontend/src/main.jsx
+++ ProjetoHamburgueria/frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CarrinhoProvider } from './context/CarrinhoContext.jsx';
+import './index.css';
+import App from './App.jsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+      <CarrinhoProvider>
+        <App />
+      </CarrinhoProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
+  
